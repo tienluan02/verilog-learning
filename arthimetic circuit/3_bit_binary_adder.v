f61386180e75f19f5c132ec7d9958e085fd43351
tienluandev <nguyentienluan3@gmail.com>
2024-06-11 15:58:44 +0700

    3_bit_binary_adder

--- arthimetic circuit/3_bit_binary_adder.v
+++ arthimetic circuit/3_bit_binary_adder.v
@@ -1,10 +1,23 @@
 module top_module( 
     input [2:0] a, b,
     input cin,
-    output [2:0] sum,
-    output cout
-);
+    output [2:0] cout,
+    output [2:0] sum );
 
-assign {cout, sum} = a + b + cin;
+    wire [2:0] c;
 
+full_adder FA0 (.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(c[0]));
+full_adder FA1 (.a(a[1]), .b(b[1]), .cin(c[0]), .sum(sum[1]), .cout(c[1]));
+full_adder FA2 (.a(a[2]), .b(b[2]), .cin(c[1]), .sum(sum[2]), .cout(c[2]));
+
+assign cout = c;
+
+endmodule
+
+module full_adder( 
+    input a, b, cin,
+    output cout, sum );
+    
+    assign sum = a^b^cin;
+    assign cout = (a&b) | (a^b)&cin;
 endmodule
--- arthimetic circuit/full_adder.v
+++ arthimetic circuit/full_adder.v
@@ -1,7 +1,7 @@
 module top_module( 
-    input [2:0] a, b,
-    input cin,
-    output [2:0] cout,
-    output [2:0] sum );
-
+    input a, b, cin,
+    output cout, sum );
+    
+    assign sum = a^b^cin;
+    assign cout = (a&b) | (a^b)&cin;
 endmodule
