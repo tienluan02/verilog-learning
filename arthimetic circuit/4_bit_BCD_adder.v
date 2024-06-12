6ae42c178bfee83782560548028246003a315762
tienluandev <nguyentienluan3@gmail.com>
2024-06-12 21:23:32 +0700

    create 4_bit_BCD adder file

---
+++ arthimetic circuit/4_bit_BCD_adder.v
@@ -0,0 +1,17 @@
+// You are provided with a BCD (binary-coded decimal) one-digit adder named bcd_fadd that adds two BCD digits and carry-in, and produces a sum and carry-out.
+
+module bcd_fadd (
+    input [3:0] a,
+    input [3:0] b,
+    input     cin,
+    output   cout,
+    output [3:0] sum );
+endmodule
+
+module top_module ( 
+    input [15:0] a, b,
+    input cin,
+    output cout,
+    output [15:0] sum );
+
+endmodule
