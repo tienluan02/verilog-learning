8e87eea6aba265b5c93b6fc44dfb8bf9f87a75d6
tienluandev <nguyentienluan3@gmail.com>
2024-06-13 09:16:47 +0700

    4-bit-BCD-adder

--- arthimetic circuit/n_bit_adder.v
+++ arthimetic circuit/n_bit_adder.v
@@ -3,7 +3,6 @@ module top_module(
     input cin,
     output cout,
     output [99:0] sum );
-    wire [99:0] c;
 
     assign {cout,sum} = a + b + cin; // automatically generate the cout at the n+1 bit, only need to include it at the beginning of the output
 endmodule
