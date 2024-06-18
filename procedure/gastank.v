de69485c4d62315d9e726efeb3252b7aaa433d5a
tienluandev <nguyentienluan3@gmail.com>
2024-06-18 12:50:09 +0700

    independent if statement

--- procedure/gastank.v
+++ procedure/gastank.v
@@ -4,16 +4,14 @@ module top_module (
     output reg shut_off_computer,
     input      arrived,
     input      gas_tank_empty,
-    output reg keep_driving  ); //
+    output reg keep_driving  
+);
 
     always @(*) begin
-        if (cpu_overheated)
-           shut_off_computer = 1;
+        shut_off_computer = cpu_overheated ? 1 : 0;
     end
 
     always @(*) begin
-        if (~arrived)
-           keep_driving = ~gas_tank_empty;
+        keep_driving = ~arrived ? ~gas_tank_empty : 0;
     end
-
 endmodule
